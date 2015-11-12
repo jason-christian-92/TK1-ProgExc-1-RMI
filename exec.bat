19f777c1679532b60928b74e820adcd660888005
Jason Christian <jason.christian.92@gmail.com>
2015-11-12 09:34:51 +0100

    new build.xml, soon-to-be-pdf file updated

--- exec.bat
+++ exec.bat
@@ -6,6 +6,3 @@ start cmd /k rmiregistry
 start cmd /k java -Djava.security.policy=policy/game.policy server.C_GameServer
 start cmd /k java -Djava.security.policy=policy/game.policy client.C_GameClient ../Util 12YearOldBoy
 start cmd /k java -Djava.security.policy=policy/game.policy client.C_GameClient ../Util 360NoScope
-start cmd /k java -Djava.security.policy=policy/game.policy client.C_GameClient ../Util Client1
-start cmd /k java -Djava.security.policy=policy/game.policy client.C_GameClient ../Util Client2
-start cmd /k java -Djava.security.policy=policy/game.policy client.C_GameClient ../Util Client3
